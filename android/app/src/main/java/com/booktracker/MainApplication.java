9ca8269648f61e14bb7fcc9e1a72cf91aa6a87d1
Andrew Gingrich <ajgingrich@yahoo.com>
2019-10-26 14:56:27 -0600

    android connect to firebase app

--- android/app/src/main/java/com/booktracker/MainApplication.java
+++ android/app/src/main/java/com/booktracker/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeFirebaseAppPackage(),
             new RNScreensPackage(),
             new ReanimatedPackage(),
             new RNGestureHandlerPackage()
